742b5d8f41c132b3f4dbc2f1d4d46a946a754441
AnnaKalini <annakalinichenko0712@gmail.com>
2025-07-20 21:56:47 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+valid_email = "ftullmbvfg@mail.ru"
+valid_password = "12345"
+
+invalid_email = "gus@vader.com"
+invalid_password = "2255888yuegg"
+
+invalid_auth_key = {'key': '555c0c8710fd04e2f53c3637407c7b1080e9f40be53e41a84dc94637'}
